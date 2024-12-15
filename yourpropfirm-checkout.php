4534b0c48c5b7d6da5c02242ed65545a4aaa7a6c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-12-15 12:36:03 +0800

    update core

---
+++ yourpropfirm-checkout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link              https://yourpropfirm.com
+ * @since             1.1.0
+ * @package           yourpropfirm-checkout
+ * GitHub Plugin URI: https://github.com/JMConsultingID/your-propfirm-addon
+ * GitHub Branch: develop
+ * @wordpress-plugin
+ * Plugin Name:       Yourpropfirm Multistep Checkout
+ * Plugin URI:        https://yourpropfirm.com
+ * Description:       This Plugin to Create multi-step checkout for WooCommerce.
+ * Version:           1.1.0
+ * Author:            YourPropfirm Team
+ * Author URI:        https://yourpropfirm.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       yourpropfirm-checkout
+ * Domain Path:       /languages
+ */

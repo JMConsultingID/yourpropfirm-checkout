2fdda382847d73f55783ddfac5431c7160bab817
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-12-20 10:32:36 +0800

    Update class-redirects.php

--- includes/class-redirects.php
+++ includes/class-redirects.php
@@ -30,17 +30,38 @@ class Yourpropfirm_Checkout_Redirects {
      * Redirect default WooCommerce checkout to the custom billing form page.
      */
     public function redirect_default_checkout() {
+        if ( class_exists( 'WooCommerce' ) ) {
         // Check if WooCommerce checkout page is being accessed.
-        if (is_checkout() && !is_wc_endpoint_url() && !isset($_GET['pay_for_order'])) {
-            // Replace with your custom billing form page URL.
-            $custom_checkout_url = home_url('/order/'); // Update the slug if necessary.
+            if (is_checkout() && !is_wc_endpoint_url() && !isset($_GET['pay_for_order'])) {
+                $custom_checkout_url = home_url('/order/');
+                // Clear WooCommerce notices.
+    	        wc_clear_notices();
+                wp_safe_redirect($custom_checkout_url);
+                exit;
+            }
 
-            // Clear WooCommerce notices.
-	        wc_clear_notices();
 
-            wp_safe_redirect($custom_checkout_url);
-            exit;
+            if ( is_page( 'cart' ) || ( isset( $_GET['cancel_order'] ) && $_GET['cancel_order'] === 'true' ) ) {
+                $home_page_url = home_url();
+                wp_safe_redirect( $home_page_url );
+                exit;
+            }
+
+            // Check if shop page is disabled
+            if ( is_shop()) {
+                $home_page_url = home_url();
+                wp_safe_redirect( $home_page_url );
+                exit;
+            }
+
+            // Check if product page is disabled
+            if ( is_product()) {
+                $home_page_url = home_url();
+                wp_safe_redirect( $home_page_url );
+                exit;
+            }
         }
+
     }
 
 }

1fbdb0aa426c05d9dab5d9223fd3641094e395bc
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-01-20 00:33:59 +0800

    Update form-billing.php

--- templates/woocommerce/checkout/form-billing.php
+++ templates/woocommerce/checkout/form-billing.php
@@ -41,6 +41,34 @@ defined( 'ABSPATH' ) || exit;
 		?>
 	</div>
 
+	<div class="col-12 mb-3 mt-5 text-center">
+	    <label class="form-label">
+	        Trading Platforms <span class="text-danger">*</span>
+	    </label>
+	    <div class="d-flex justify-content-center gap-4">
+	        <div class="form-check">
+	            <input class="form-check-input mt-3 me-3" type="radio" name="yourpropfirm_mt_version" id="yourpropfirm_mt_version_MT4" value="MT4" checked>
+	            <label class="form-check-label" for="yourpropfirm_mt_version_MT4">
+	                MT4
+	            </label>
+	        </div>
+	        
+	        <div class="form-check">
+	            <input class="form-check-input mt-3 me-3" type="radio" name="yourpropfirm_mt_version" id="yourpropfirm_mt_version_MT5" value="MT5">
+	            <label class="form-check-label" for="yourpropfirm_mt_version_MT5">
+	                MT5
+	            </label>
+	        </div>
+	        
+	        <div class="form-check">
+	            <input class="form-check-input mt-3 me-3" type="radio" name="yourpropfirm_mt_version" id="yourpropfirm_mt_version_CTrader" value="CTrader">
+	            <label class="form-check-label" for="yourpropfirm_mt_version_CTrader">
+	                CTrader
+	            </label>
+	        </div>
+	    </div>
+	</div>	
+
 	<?php do_action( 'woocommerce_after_checkout_billing_form', $checkout ); ?>
 </div>
 
